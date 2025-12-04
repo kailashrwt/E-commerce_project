713dd0b592c1ae9cdcba3cb1d2af2bf17fbfa712
kailashrwt <krwt2277@gmail.com>
2025-12-04 18:54:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000', "https://lara-jewellery-e-commerce.netlify.app"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
