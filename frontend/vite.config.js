397ec8b15064d1340f84396a7578455ba6498c83
kailashrwt <krwt2277@gmail.com>
2025-12-06 22:26:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-base : process.env.NODE_EVN === "Production
-? '/E-commerce_project/'
-plugins: [react()],
-
+  plugins: [react()],
+  base: '/E-commerce_project/',   // Correct base path for GitHub Pages
 })
