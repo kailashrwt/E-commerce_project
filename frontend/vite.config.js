7404b819453a2631fbf33aa0dede7f7f4d38f514
kailashrwt <krwt2277@gmail.com>
2025-12-06 14:07:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
 base : process.env.NODE_EVN === "Production
-? '/E-commerce_project'
+? '/E-commerce_project/'
+plugins: [react()],
+
 })
